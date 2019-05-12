8c07b1f5c117a136ff33f4d2e3b3d07f4eda884f
Harry Merritt <harry@merritt.es>
2019-05-12 23:20:41 +0100

    removed medium batch install script
    - removed medium script
    - sorted programs into categorys

--- 2. chocolatey/startup-install--heavy.bat
+++ 2. chocolatey/startup-install--heavy.bat
@@ -2,51 +2,90 @@
 echo Installing apps (Heavy)
 
 
+
+
+#  essential
+
 choco install -y 7zip
+choco install -y aria2
 choco install -y googlechrome
 choco install -y irfanview
+choco install -y mpc-hc
 choco install -y notepadplusplus
 choco install -y putty
-choco install -y aria2
 
+
+
+
+#  media
+
+choco install -y foobar2000
+choco install -y irfanviewplugins
+choco install -y plexmediaplayer
+choco install -y vlc
+choco install -y webtorrent-desktop
+
+
+
+
+#  social
+
+choco install -y discord
+choco install -y steam
+
+
+
+
+#  coding
 
 choco install -y atom
-choco install -y everything
-choco install -y f.lux
-choco install -y git
 choco install -y gitkraken
 choco install -y hyper
-choco install -y irfanviewplugins
-choco install -y mpc-hc
-choco install -y mremoteng
-choco install -y openvpn
+choco install -y nodejs
 choco install -y python3
-choco install -y spek
-choco install -y winscp
+choco install -y redis
+choco install -y mremoteng
 
+
+
+
+#  cli
+
+choco install -y curl
+choco install -y ffmpeg
+choco install -y git
+choco install -y youtube-dl
+
+
+
+
+# securaty
+
+choco install -y keeweb
+choco install -y openvpn
+
+
+
+
+#  tools
 
 choco install -y audacity
-choco install -y curl
-choco install -y discord
-choco install -y ffmpeg
-choco install -y foobar2000
+choco install -y everything
+choco install -y f.lux
 choco install -y google-backup-and-sync
 choco install -y handbrake
-choco install -y keeweb
 choco install -y malwarebytes
 choco install -y mp3tag
-choco install -y nodejs
-choco install -y plexmediaplayer
 choco install -y speccy
-choco install -y steam
+choco install -y spek
 choco install -y syncthing
 choco install -y sysinternals
 choco install -y unifiedremote
 choco install -y veracrypt
-choco install -y vlc
-choco install -y webtorrent-desktop
+choco install -y winscp
 choco install -y wireshark
-choco install -y youtube-dl
+
+
 
 
 refreshenv
--- 2. chocolatey/startup-install--light.bat
+++ 2. chocolatey/startup-install--light.bat
@@ -2,11 +2,19 @@
 echo Installing apps (Light)
 
 
+
+
+#  essential
+
 choco install -y 7zip
+choco install -y aria2
 choco install -y googlechrome
 choco install -y irfanview
+choco install -y mpc-hc
 choco install -y notepadplusplus
 choco install -y putty
+
+
 
 
 refreshenv
