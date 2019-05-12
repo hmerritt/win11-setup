85c42b7b6629152a3c0de11088ce9f015edf4483
Harry Merritt <harry@merritt.es>
2019-05-12 23:22:18 +0100

    added an update batch script for all programs

---
+++ 2. chocolatey/update/update--light.bat
@@ -0,0 +1,20 @@
+
+echo updateing apps (Light)
+
+
+
+
+#  essential
+
+choco update -y 7zip
+choco update -y aria2
+choco update -y googlechrome
+choco update -y irfanview
+choco update -y mpc-hc
+choco update -y notepadplusplus
+choco update -y putty
+
+
+
+
+refreshenv
