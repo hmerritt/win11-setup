75e781bd5fa106f021926b63f5ba4dd706ce20a1
hmerritt <harry@mrrtt.me>
2022-06-22 20:18:53 +0100

    Major changes to scoop install list

--- 2. install software/software-install.ps1
+++ 2. install software/software-install.ps1
@@ -19,54 +19,73 @@ scoop install mpv
 #scoop install webtorrent
 
 
-#  social
-scoop install discord
-#scoop install keybase
-scoop install steam
-
-
 #  programming
-#scoop install atom
 #scoop install gitkraken
-#scoop install mremoteng
-scoop install vscode
 scoop install insomnia
+#scoop install ten-hands
+scoop install vscode
 
 #  programming languages
 scoop install go
-scoop install tinygo
-scoop install nodejs
+scoop install lua
+scoop install nvm # nodejs version manager. Run `nvm install` as admin
+scoop install php
 scoop install python
+scoop install rust
+scoop install tinygo
 
 
 #  cli
 scoop install aria2
+scoop install composer
 scoop install curl
+scoop install dezoomify-rs
 scoop install ffmpeg
+scoop install fspop
 scoop install git
+scoop install jq
+scoop install mediainfo
+scoop install nssm # the Non-Sucking Service Manager
 scoop install youtube-dl
+scoop install yt-dlp
 
 
 # security
 scoop install keeweb
 scoop install malwarebytes
-scoop install openvpn
 scoop install veracrypt
+scoop install wireguard-np
 
 
 #  tools
 scoop install audacity
+scoop install blender
+scoop install bulk-rename-utility
+scoop install crystaldiskinfo
+scoop install CrystalDiskMark
+scoop install dupeguru
 scoop install everything
+scoop install flac
 scoop install flux
 #scoop install google-backup-and-sync
 scoop install handbrake
+scoop install losslesscut
+scoop install mkvtoolnix
 scoop install mp3tag
+scoop install rufus
+scoop install scoop-search
+scoop install soulseekqt
 scoop install speccy
 scoop install spek
+scoop install sshfs-np
+scoop install starship
 #scoop install syncthing
 scoop install sysinternals
 #scoop install unifiedremote
+scoop install windirstat
+scoop install winfsp-np
 scoop install winscp
+scoop install xnconvert
 
 
 refreshenv
