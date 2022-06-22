2f4cc0e1b3dfc8adf6df48058d7ef382402b3e8a
hmerritt <harry@mrrtt.me>
2022-06-22 20:25:26 +0100

    Script for pinning scoop software updates

---
+++ 2. install software/software-pin.ps1
@@ -0,0 +1,15 @@
+
+echo "Pinning software that updates (too) frequently"
+
+
+scoop hold blender
+scoop hold brave
+scoop hold crystaldiskinfo
+scoop hold CrystalDiskMark
+scoop hold gcc
+scoop hold git
+scoop hold go
+scoop hold make
+scoop hold nssm
+scoop hold php
+scoop hold python
