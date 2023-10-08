a3be9ba714191107146f31c032c007d8ae3fb33e
hmerritt <harry@mrrtt.me>
2023-10-08 13:42:46 +0100

    feat: custom encode function

--- 2. install software/profiles/Microsoft.PowerShell_profile.ps1
+++ 2. install software/profiles/Microsoft.PowerShell_profile.ps1
@@ -21,6 +21,37 @@ Set-Alias yt yt-dlp
 
 
 ### >>>>>>>>>          Custom functions        <<<<<<<<< ###
-#function dl {
-#  aria2c @args
-#}
+function encode {
+	#
+	# Quickly encode video with `hevc_nvenc`
+	#
+	[CmdletBinding()]
+    param(
+        [string]$inputVideo = "",
+        [int]$qp = 25,
+		[string]$outputVideo = ""
+    )
+
+    if ([string]::IsNullOrWhiteSpace($outputVideo)) {
+        $outputVideo = "output.${qp}.mp4"
+    }
+	
+	Write-Host "Encode [hevc_nvenc/${qp} ${inputVideo} -> ${outputVideo}]"
+	Write-Host ""
+
+    if ([string]::IsNullOrWhiteSpace($inputVideo)) {
+		Write-Host "ERROR: Input video not provided"
+		Write-Host ""
+		Write-Host "Help:"
+		Write-Host "$ encode <input file> <quality level (15-40)> <output file>"
+		Write-Host "$ encode input.mp4 25 output.mp4"
+        return
+    }
+
+	Write-Host "Input:   ${inputVideo}"
+	Write-Host "Quality: ${qp}"
+	Write-Host "Output:  ${outputVideo}"
+	Write-Host ""
+
+    ffmpeg -i "${inputVideo}" -c:a copy -c:v hevc_nvenc -qp "${qp}" -preset p7 -multipass fullres "${outputVideo}"
+}
