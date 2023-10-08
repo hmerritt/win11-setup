8197c6097c32fcdef797e255533d5f00bb5374f0
hmerritt <harry@mrrtt.me>
2023-10-08 12:44:38 +0100

    feat: cmd profile

---
+++ 2. install software/profiles/Microsoft.Command_processor.bat
@@ -0,0 +1,11 @@
+@echo off
+::
+:: CMD set profile.
+::
+:: 1. Regedit `Computer\HKEY_CURRENT_USER\Software\Microsoft\Command Processor`
+:: 2. Add key `Autorun` with string value with the location of this file
+
+
+:: >>>>>>>>>              Aliases             <<<<<<<<< ::
+DOSKEY dl=aria2c $*
+DOSKEY yt=yt-dlp $*
